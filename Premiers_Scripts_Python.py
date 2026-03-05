cf6f1a4cb63d8d949aac8b365e2f303ab7f133c8
Adam BRASSET <adam.brasset@ldu.nc>
2026-03-05 14:03:55 +1100

    Fichier Script : Suppréssion des Fonctions

--- Premiers_Scripts_Python.py
+++ Premiers_Scripts_Python.py
@@ -1,10 +1,10 @@
-def affichage():
+
     print("Hello World !")
 
-def Entrée():
+
     print(f"Vous avez écris : \"{input("Insérez quelque chose : ")}\"")
 
-def Calcul():
+
     while True:
         try:
             a = int(input("Nombre 1 : "))
@@ -19,7 +19,7 @@ def Calcul():
             print("Veuillez entrer un entier valide.")
     print("Le résultat de l'addition est : ", a + b)
 
-def CalculFloat():
+
     while True:
         try:
             a = float(input("Nombre 1 : "))
@@ -34,7 +34,7 @@ def CalculFloat():
             print("Veuillez entrer un nombre valide.")
     print("La moyenne est : ", (a + b) / 2)
 
-def Conversion_CfpToEuro():
+
     while True:
         try:
             cfp = float(input("Montant en CFP : "))
@@ -44,7 +44,6 @@ def Conversion_CfpToEuro():
     euro = cfp / 119.33
     print(f"{cfp} CFP équivaut à {euro:.2f} Euros.")
 
-def Conversion_EuroToCfp():
     while True:
         try:
             euro = float(input("Montant en Euros : "))
